2fc145578d16f9a527757367a7f0f7b644842b34
Michael Beckwith <tw2113@gmail.com>
2021-02-15 20:01:54 -0600

    remove server side part

--- src/edit.js
+++ src/edit.js
@@ -4,7 +4,6 @@ import { __ } from '@wordpress/i18n';
  * @see https://developer.wordpress.org/block-editor/packages/packages-block-editor/#useBlockProps
  */
 import { useBlockProps, InspectorControls } from '@wordpress/block-editor';
-import ServerSideRender from '@wordpress/server-side-render';
 import './editor.scss';
 
 import {
